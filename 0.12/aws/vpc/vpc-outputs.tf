c631a5b84020a1955d244bd6707c41105755fc8f
Arkadiusz Czajkowski <arkadiuszczajkowski@gmail.com>
2020-05-14 10:21:47 +0200

    Temp resources to work on AWS Demo

--- 0.12/aws/vpc/vpc-outputs.tf
+++ 0.12/aws/vpc/vpc-outputs.tf
@@ -9,11 +9,24 @@ output "vpc" {
 
 # Subnets
 output "private_subnets" {
-  value = aws_subnet.private
+  value = local.private_subnets_map
 }
 
 output "public_subnets" {
   value = aws_subnet.public
+}
+
+# VPC Peering
+output "vpc_pcxs" {
+  value = aws_vpc_peering_connection.vpc_peer_conn
+}
+
+output "vpc_pcxs_routing" {
+  value = aws_route.vpc_peer_route
+}
+
+output "vpc_pcxs_reverse_routing" {
+  value = aws_route.vpc_peer_reverse_route
 }
 
 # NAT
